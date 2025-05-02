4490fbe42f5d5d4966a5ab29608db0b02d26294f
Avumile7 <gxulaavumile@gmail.com>
2025-05-02 09:48:52 +0000

    Update MainActivity.kt

--- app/src/main/java/com/example/quezapp/MainActivity.kt
+++ app/src/main/java/com/example/quezapp/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.quezapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState : Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+       //val welcomeText: TextView = findViewById(R.id.textView)
+        val startButton: Button = findViewById(R.id.button)
+
+        startButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
         }
     }
 }
+
